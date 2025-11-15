53f17c1b22d54ecb511e8ea6794f83476742d0ac
bbigimania <seation@nate.com>
2025-11-15 14:56:40 +0900

    251115

---
+++ js05.js
@@ -0,0 +1,31 @@
+// # 함수(Function)
+
+//선언적 함수
+
+console.log(add(5, 7));
+
+function add(a, b) {
+  //함수 선언
+  let result = a + b;
+  return result;
+}
+
+// 함수를 호출한다.
+
+let addResult = add(3, 2);
+console.log(addResult);
+
+//익명적 함수
+//콜백 함수
+let plus = function (a, b) {
+  return a + b;
+};
+
+console.log(plus(4, 2));
+
+//화살표 함수
+let plus1 = (a, b) => {
+  return a + b;
+};
+
+console.log(plus1(5, 2));
